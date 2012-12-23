fc0f93bca2ba351675515fb009e57b74c455e390
Alex Basson <alexbasson@gmail.com>
2012-12-23 07:54:19 -0500

    Added ABHorizontalPicker subclass of UIView.

---
+++ ABHorizontalPicker/ABHorizontalPicker.m
@@ -0,0 +1,31 @@
+//
+//  ABHorizontalPicker.m
+//  ABHorizontalPicker
+//
+//  Created by Alex on 12/23/12.
+//  Copyright (c) 2012 Alex Basson. All rights reserved.
+//
+
+#import "ABHorizontalPicker.h"
+
+@implementation ABHorizontalPicker
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
